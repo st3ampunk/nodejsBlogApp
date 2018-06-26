b38c083530eb3295c16c9f13fd354a2dfe8f03d4
Abishek Babu <abishekbabu000@gmail.com>
2018-06-26 07:19:25 +0530

    first Commit
    first Commit

---
+++ routes/categories.js
@@ -0,0 +1,58 @@
+var express = require("express");
+var router = express.Router();
+var mongo = require("mongodb");
+var db = require("monk")("localhost/nodeblog");
+
+router.get("/show/:category", function(req, res, next) {
+  var db = req.db;
+  var posts = db.get("posts");
+  posts.find({ category: req.params.category }, {}, function(err, posts) {
+    res.render("index", {
+      title: req.params.category,
+      posts: posts
+    });
+  });
+});
+
+router.get("/add", function(req, res, next) {
+  res.render("addcategory", {
+    title: "Add Category"
+  });
+});
+
+router.post("/add", function(req, res, next) {
+  // Get form values
+  var title = req.body.title;
+
+  // Form validation
+  req.checkBody("title", "Title field is required").notEmpty();
+
+  var errors = req.validationErrors();
+
+  if (errors) {
+    res.render("addcategory", {
+      errors: errors,
+      title: title
+    });
+  } else {
+    var categories = db.get("categories");
+
+    // Submit to database
+    categories.insert(
+      {
+        title: title
+      },
+      function(err, category) {
+        if (err) {
+          res.send("There was an issue submitting the category");
+        } else {
+          req.flash("success", "Category Submitted");
+          res.location("/");
+          res.redirect("/");
+        }
+      }
+    );
+  }
+});
+
+module.exports = router;
---
+++ routes/posts.js
@@ -0,0 +1,139 @@
+var express = require("express");
+var router = express.Router();
+var mongo = require("mongodb");
+var db = require("monk")("localhost/nodeblog");
+
+router.get("/show/:id", function(req, res, next) {
+  var posts = db.get("posts");
+  posts.findById(req.params.id, function(err, post) {
+    res.render("show", {
+      post: post
+    });
+  });
+});
+
+router.get("/add", function(req, res, next) {
+  var categories = db.get("categories");
+
+  categories.find({}, {}, function(err, categories) {
+    res.render("addpost", {
+      title: "Add Post",
+      categories: categories
+    });
+  });
+});
+
+router.post("/add", function(req, res, next) {
+  // Get form values
+  var title = req.body.title;
+  var category = req.body.category;
+  var body = req.body.body;
+  var author = req.body.author;
+  var date = new Date();
+
+  if (req.files.mainimage) {
+    var mainImageOriginalName = req.files.mainimage.originalname;
+    var mainImageName = req.files.mainimage.name;
+    var mainImageMime = req.files.mainimage.mimetype;
+    var mainImagePath = req.files.mainimage.path;
+    var mainImageExt = req.files.mainimage.extension;
+    var mainImageSize = req.files.mainimage.size;
+  } else {
+    var mainImageName = "noimage.png";
+  }
+
+  // Form validation
+  req.checkBody("title", "Title field is required").notEmpty();
+  req.checkBody("body", "Body field is required");
+
+  var errors = req.validationErrors();
+
+  if (errors) {
+    res.render("addpost", {
+      errors: errors,
+      title: title,
+      body: body
+    });
+  } else {
+    var posts = db.get("posts");
+
+    // Submit to database
+    posts.insert(
+      {
+        title: title,
+        body: body,
+        category: category,
+        date: date,
+        author: author,
+        mainimage: mainImageName
+      },
+      function(err, post) {
+        if (err) {
+          res.send("There was an issue submitting the post");
+        } else {
+          req.flash("success", "Post Submitted");
+          res.location("/");
+          res.redirect("/");
+        }
+      }
+    );
+  }
+});
+
+router.post("/addcomment", function(req, res, next) {
+  // Get form values
+  var name = req.body.name;
+  var email = req.body.email;
+  var body = req.body.body;
+  var postid = req.body.postid;
+  var commentdate = new Date();
+
+  // Form validation
+  req.checkBody("name", "Name field is required").notEmpty();
+  req.checkBody("email", "Email field is required").notEmpty();
+  req.checkBody("email", "Email is incorrectly formatted").isEmail();
+  req.checkBody("body", "Body field is required").notEmpty();
+
+  var errors = req.validationErrors();
+
+  if (errors) {
+    var posts = db.get("posts");
+    posts.findById(postid, function(err, post) {
+      res.render("show", {
+        errors: errors,
+        post: post
+      });
+    });
+  } else {
+    var comment = {
+      name: name,
+      email: email,
+      body: body,
+      commentdate: commentdate
+    };
+
+    var posts = db.get("posts");
+
+    posts.update(
+      {
+        _id: postid
+      },
+      {
+        $push: {
+          comments: comment
+        }
+      },
+      function(err, doc) {
+        if (err) {
+          throw err;
+        } else {
+          req.flash("success", "Comment added");
+          res.location("/posts/show/" + postid);
+          res.redirect("/posts/show/" + postid);
+        }
+      }
+    );
+  }
+});
+
+module.exports = router;
